805d56b6394706162a8a00d60503e4352de2c606
nadia2612 <YOUR@EMAIL.com>
2020-01-09 15:08:25 +0100

    Add search conference by name and description

--- conference/router.js
+++ conference/router.js
@@ -6,17 +6,26 @@ const router = new Router();
 const Comment=require("../comment/model")
 const Favourite=require("../favourite/model")
 const Location=require("../location/model")
+const User =require("../user/model")
 
 
 router.get("/conference", async (req, res, next) => {
   try {
+    console.log("search string", req.query.search);
     const conferences = await Conference.findAll({
       offset: req.query.offset,
       limit: req.query.limit,
       where: {
         end_date: {
           [Op.gte]: new Date()
-        }
+        },
+        [Op.or]: [{
+        name: {
+          [Op.like]: `%${req.query.search}%`
+        }},
+         {
+          description:
+          {[Op.like]: `%${req.query.search}%`}}]
       }, include:[{model:Location, attributes:["city","country"]}]
     });
     res.send(conferences);
@@ -28,7 +37,7 @@ router.get("/conference", async (req, res, next) => {
 router.get("/conference/:id", async (req, res, next) => {
   try {
     const conference = await Conference.findByPk(req.params.id, {
-      include: [{ model: Location }]
+      include: [{ model: Location},{model: Comment,include: [User]}, {model:Favourite }]
     });
     res.send(conference);
   } catch (error) {
